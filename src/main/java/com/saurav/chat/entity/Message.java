8e5883bc99337fdb16cc66b6640bfabb88076d28
Saurav-KumarSingh <sauravkumarsingh093@gmail.com>
2025-10-25 14:32:36 +0530

    room entity and repo

---
+++ src/main/java/com/saurav/chat/entity/Message.java
@@ -0,0 +1,28 @@
+package com.saurav.chat.entity;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message {
+
+    private Long sender;
+    private String content;
+    private LocalDateTime timeStamp;
+
+    public Message(Long sender, String content) {
+        this.sender = sender;
+        this.content = content;
+        this.timeStamp = LocalDateTime.now();
+    }
+
+
+}
---
+++ src/main/java/com/saurav/chat/entity/Room.java
@@ -0,0 +1,28 @@
+package com.saurav.chat.entity;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "rooms")
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String roomId;
+    private List<Message> messages=new ArrayList<>();
+
+}
